4c3be6ae7e04724cefa2c6e4f8f30b6a32b7ccf3
David Wendt <dcrkid@yahoo.com>
2019-01-27 12:06:48 -0700

    Retire the PartialResult facility as it was only useful for stream, which is also retired.

--- src/rapidtar/mod.rs
+++ src/rapidtar/mod.rs
@@ -18,6 +18,3 @@ pub mod normalize;
 
 /// Facilities for tracking data within a write buffer for error recovery.
 pub mod spanning;
-
-/// Basic result types for functions which can partially succeed.
-pub mod result;
